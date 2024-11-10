684f944da9498c57c5fe3b1f047df5cf525dc2e4
WebWolf77 <tommyecom888@gmail.com>
2024-11-10 22:22:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+// Select the menu icon and navbar elements
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
 
+// Handle scroll event to highlight navigation links
 window.onscroll = () => {
+    let sections = document.querySelectorAll('section');
+    let navLinks = document.querySelectorAll('header nav a');
+
     sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
@@ -11,13 +14,15 @@ window.onscroll = () => {
         let id = sec.getAttribute('id');
 
         if (top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a [href*=' + id + ' ]').classList.add('active')
-            })
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
         }
-    })
+    });
 }
+
+// Toggle navbar and menu icon on click
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
